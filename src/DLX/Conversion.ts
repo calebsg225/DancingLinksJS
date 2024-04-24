c5a3315c057ba82709d2015042136bcc59be861f
caleb <calebsg2253284@gmail.com>
2024-04-24 02:39:40 -0400

    matrix conversion progress

--- src/DLX/Conversion.ts
+++ src/DLX/Conversion.ts
@@ -1,6 +1,16 @@
 
 type NodeType = 'header' | 'spacer' | 'item' | 'first';
 
+type NodeTypes = {
+  nodeType?: NodeType;
+  columnCount?: number;
+  upNode?: number;
+  downNode?: number;
+  leftNode?: number;
+  rightNode?: number;
+  headerNode?: number;
+}
+
 class Node {
   nodeType: NodeType;
   upNode: number;
@@ -58,7 +68,7 @@ class Convert {
     const colCount = matrix[0].length; // number of columns in the initial matrix
     const rowCount = matrix.length; // number of rows in the initial matrix
 
-    const nodes = [];
+    const nodes: NodeTypes[] = [];
 
     // create first node
     nodes.push(new FirstNode(1, colCount));
@@ -66,6 +76,23 @@ class Convert {
     // create header nodes, each pointing to themselves in the up and down direction
     for (let i = 1; i <= colCount; i++) {
       nodes.push(new HeaderNode(i-1, i === colCount ? 0 : i+1, i, i));
+    }
+
+    // create the rest of the relevant nodes, i.e. spacer and item nodes
+    for (let i = 0; i < rowCount; i++) {
+      // create new spacer node pointing to itself in all directions
+      nodes.push(new SpacerNode(nodes.length, nodes.length));
+      const spacerIndex = nodes.length - 1;
+      for (let j = 0; j < colCount; j++) {
+        if (!matrix[i][j]) continue; // skip if value is 0
+        nodes[spacerIndex].downNode = nodes.length; // update spacer pointer
+
+        nodes[nodes[j+1].upNode].downNode = nodes.length; // update header down pointer
+        nodes[j+1].upNode = nodes.length; // update header up pointer
+        nodes[j+1].columnCount++; // increase header column count
+        
+        nodes.push(new ItemNode(j+1, nodes[j+1].upNode, j+1)); // add new item node to nodes array
+      }
     }
   }
 
