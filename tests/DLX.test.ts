c186f1d20d9bfdb11d5ab8cd29b622711f3fab6e
caleb <calebsg2253284@gmail.com>
2024-04-24 17:57:01 -0400

    clean up

--- tests/DLX.test.ts
+++
@@ -1,8 +0,0 @@
-import DancingLinks from "../src/DLX/DLX";
-
-describe('testing DLX file', () => {
-  const dlx = new DancingLinks();
-  test(`should convert string to number`, () => {
-    expect(dlx.test('69')).toBe(69);
-  });
-});
